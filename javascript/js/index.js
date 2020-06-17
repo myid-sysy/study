0db42baf22ef2d3ed5d5af6b1e35d8b1923d444e
박상연 <park-sangyeon@bagsang-yeon-ui-MacBook-Pro.local>
2020-06-17 16:57:27 +0900

    1-8 data types on JS
    String, Boolean, Number, Float

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,14 +1,30 @@
 // alert('Im Working. Im JS. Im Beautiful. Im Worth it')
 //console.log('Im Working. Im JS. Im Beautiful. Im Worth it')
 
-
+/*
 const a = 221; //a를 생성하고, 숫자 221로 초기화.
 let b = a - 5;
 a = 4;
 console.log(b ,a);
+*/
  //  모든 expressions는 한 둘에 있어야 됨. 그리고 끝나는 곳을 선언하는 방법 ';'세미콜론 을 사용
  //  1) Create 생성 
  //  2) Initialize 초기화 필요할 때는 생성과 초기화를 동시에 함
  //  3) Use 사용
- // let => 변수를 초기화하거나 생성할 떄에는 let을 써야하지만 사용할 떄는 쓰지않아도 됨.
-// const = 상수. 변하지 않음
+ // let =>  변수를 초기화하거나 생성할 떄에는 let을 써야하지만 사용할 떄는 쓰지않아도 됨.
+ // const = 상수. 변하지 않음 
+ //Assignment to constant variable. => 상수 변수에 대입. 에러 메세지가 뜸.
+
+// String => text 큰 따옴표("")안에 작성하면 텍스트로 인식
+const what = "SY";
+console.log(what);
+
+//Boolean
+const wat = true; //모두 소문자로 작성하고 ""없음. 텍스트가 아님
+// true = 1, false = 0
+
+//Number
+const num = 666;
+
+//Float  floating number(떠돌이 소숫점)을 가지고 있음.
+const dot = 55.5;
