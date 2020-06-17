3c6fde9fffb032981550bc6e02bbea14745fd41a
박상연 <park-sangyeon@bagsang-yeon-ui-MacBook-Pro.local>
2020-06-17 16:04:46 +0900

    1-7)let, const, var
    let ->변수를 생성하거나 초기화할 때는 사용(변수가 변하는 걸 허용)
    const -> 상수 (변하지 않음)
    var - 요즘은 사용하지 않음

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -2,7 +2,7 @@
 //console.log('Im Working. Im JS. Im Beautiful. Im Worth it')
 
 
-let a = 221; //a를 생성하고, 숫자 221로 초기화.
+const a = 221; //a를 생성하고, 숫자 221로 초기화.
 let b = a - 5;
 a = 4;
 console.log(b ,a);
@@ -11,3 +11,4 @@ console.log(b ,a);
  //  2) Initialize 초기화 필요할 때는 생성과 초기화를 동시에 함
  //  3) Use 사용
  // let => 변수를 초기화하거나 생성할 떄에는 let을 써야하지만 사용할 떄는 쓰지않아도 됨.
+// const = 상수. 변하지 않음
