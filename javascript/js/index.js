c7bd595062b48ba9d631e859d75b0c653a0e4cda
박상연 <park-sangyeon@bagsang-yeon-ui-MacBook-Pro.local>
2020-06-17 15:43:27 +0900

    1-6) Variables
     모든 expressions는 한 둘에 있어야 됨. 그리고 끝나는 곳을 선언하는 방법 ';'세미콜론 을 사용
     1) Create 생성
     2) Initialize 초기화 필요할 때는 생성과 초기화를 동시에 함
     3) Use 사용
     let => 변수를 초기화하거나 생성할 떄에는 let을 써야하지만 사용할 떄는 쓰지않아도 됨.

---
+++ javascript/js/index.js
@@ -0,0 +1,13 @@
+// alert('Im Working. Im JS. Im Beautiful. Im Worth it')
+//console.log('Im Working. Im JS. Im Beautiful. Im Worth it')
+
+
+let a = 221; //a를 생성하고, 숫자 221로 초기화.
+let b = a - 5;
+a = 4;
+console.log(b ,a);
+ //  모든 expressions는 한 둘에 있어야 됨. 그리고 끝나는 곳을 선언하는 방법 ';'세미콜론 을 사용
+ //  1) Create 생성 
+ //  2) Initialize 초기화 필요할 때는 생성과 초기화를 동시에 함
+ //  3) Use 사용
+ // let => 변수를 초기화하거나 생성할 떄에는 let을 써야하지만 사용할 떄는 쓰지않아도 됨.
