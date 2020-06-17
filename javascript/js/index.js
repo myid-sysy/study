2c2caa8d9ecfb7995348fa903bab646461c2ac19
myid-sysy <60874174+myid-sysy@users.noreply.github.com>
2020-06-17 17:32:49 +0900

    Update index.js

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,12 +1,12 @@
 // alert('Im Working. Im JS. Im Beautiful. Im Worth it')
 //console.log('Im Working. Im JS. Im Beautiful. Im Worth it')
 
-/*
+
 const a = 221; //a를 생성하고, 숫자 221로 초기화.
 let b = a - 5;
 a = 4;
 console.log(b ,a);
-*/
+
  //  모든 expressions는 한 둘에 있어야 됨. 그리고 끝나는 곳을 선언하는 방법 ';'세미콜론 을 사용
  //  1) Create 생성 
  //  2) Initialize 초기화 필요할 때는 생성과 초기화를 동시에 함
