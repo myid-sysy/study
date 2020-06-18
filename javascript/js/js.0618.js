45c10e4ea62c83a82dc02d69ce98b645cfd2646e
박상연 <park-sangyeon@bagsang-yeon-ui-MacBook-Pro.local>
2020-06-18 16:38:20 +0900

    2-2~2-5
    DOM Function
    Events and event handlers
    if ~ else / AND(&&) / OR(||)

--- javascript/js/js.0618.js
+++ javascript/js/js.0618.js
@@ -24,3 +24,102 @@ console object 안에 있는 함수라는 뜻
 ("넣고 싶은 것을 두개 따옴표 안에 넣으면 함수는 그걸 이용함.")
 인자(argument) -> 변수 같은 거 우리가 주는 값은 저장함.함수 우리가 주는 argument를 가지고 갈 수 있게? sayHello를 준비시켜야 됨.  
 */
+
+
+
+// JS DOM Function
+const title = document.getElementById("title");
+title.innerHTML= "Hi! From JS";
+title.style.color = "white";
+document.title = "I own you now";
+
+
+// const selector = document.querySelector("#title"); //css 선택자와 비슷함.
+// title.innerHTML= "Hi! From JS";
+// title.style.color = "red";
+// document.title = "I own you now";
+// console.dir(document);
+
+
+/*
+DOM = Document Object Module
+JS는 HTML있는 모든 요소를 가져오고 그리고 그걸 객체로(object) 바꿈
+*/
+
+/*
+Events and event handlers
+*/
+
+const selector = document.querySelector("#title");
+
+function handleClick(){
+    // console.log(" I have been resized");
+    title.style.color = "red";
+}
+title.addEventListener("click", handleClick); //윈도우 사이즈가 변화될 떄 handleResize 함수를 호출하자~!
+
+
+// window.addEventListener("resize", handleResize) -> 필요한 시점에서 함수를  호출하는거 
+// window.addEventListener("resize", handleResize()) -> 지금 당장 함수를 호출하는거
+
+
+/*
+2-5
+if-else 
+if(condition조건){ // 조건은 항상 true
+    block  //true
+} else {
+    block //false
+}
+*/
+if ("10" === 10){ //문자열과 숫자는 같지 않음.
+    console.log('hi');
+} else if("10" === "10"){
+    console.log("lalala");
+} else {
+    console.log("ho");
+}
+
+/*
+AND = &&
+true &&  true = true;
+true && false =false;
+false && true = false;
+false && false = false;
+둘 중 하나라도 거짓이만 false
+*/
+
+if(20 >5 && "nicolas" === "nicolas"){
+    console.log('yes');
+} else {
+    console.log('no');
+}
+
+/*
+OR= ||
+true ||  true = true;
+true || false = true;
+false ||  true = true;
+false ||  false = false;
+하나 혹운 나머지 것 을 뜻함.
+
+*/
+
+
+if(20 >5 ||"niolas" === "nicolas"){
+    console.log('yes');
+} else {
+    console.log('no');
+}
+
+const age =  prompt("How old are you"); 
+
+if (age >= 18 && age <= 21){
+    console.log("you can drink but you should not");
+} else if(age > 21){
+    console.log("go ahed");
+} else {
+    console.log("too young");
+}
+
+console.log(age);
